5d479369e0fac4414637309c36efd2e3ea8c08ed
puriinz <puriinz@puriinzs-MacBook-Pro.local>
2022-10-15 02:29:45 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,83 @@
-import Login from './views/Login';
-import NotFound from './views/NotFound';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Login from './views/Login';
+// import NotFound from './views/NotFound';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-function App() {
+// function App() {
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route exact path='/' element={<Login />} />
+//           <Route exact path='*' element={<NotFound />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import logo from '../logo.svg';
+import '../App.css';
+import liff from '@line/liff';
+import { useEffect, useState } from 'react';
+function Login() {
+
+  const [pictureUrl, setPictureUrl] = useState(logo);
+  const [idToken, setIdToken] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const logout = () => {
+    liff.logout();
+    window.location.reload();
+  }
+
+  const initLine = () => {
+    liff.init({ liffId: '1657559918-NqzQz1GA' }, () => {
+      if (liff.isLoggedIn()) {
+        runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  const runApp = () => {
+    const idToken = liff.getIDToken();
+    setIdToken(idToken);
+    liff.getProfile().then(profile => {
+      setDisplayName(profile.displayName);
+      setPictureUrl(profile.pictureUrl);
+      setStatusMessage(profile.statusMessage);
+      setUserId(profile.userId);
+      // InsertProfile(idToken, profile)
+    }).catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    initLine();
+  }, []);
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<Login />} />
-          <Route exact path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <div style={{ textAlign: "center" }}>
+          <h1>React with LINE Login test bot</h1>
+          <hr />
+          <img src={pictureUrl} width="300px" height="300px" />
+          <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>id token: </b> {idToken}</p>
+          <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
+          <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>status message: </b> {statusMessage}</p>
+          <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
+
+          <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
+        </div>
+      </header>
+    </div>
   );
 }
 
-export default App;
+export default Login;
