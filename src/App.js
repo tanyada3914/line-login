cae4a39282325dcb711a58783450432dc1fea879
puriinz <puriinz@puriinzs-MacBook-Pro.local>
2022-10-15 01:19:00 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
 import liff from '@line/liff';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [pictureUrl, setPictureUrl] = useState(logo)
-  const [idToken, setIdToken] = useState('')
-  const [displayName, setDisplayName] = useState('')
-  const [statusMessage, setStatusMessage] = useState('')
-  const [userId, setUserId] = useState('')
+  const [pictureUrl, setPictureUrl] = useState(logo);
+  const [idToken, setIdToken] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
+  const [userId, setUserId] = useState("");
 
   const logout = () => {
     liff.logout();
@@ -17,7 +17,7 @@ function App() {
   }
 
   const initLine = () => {
-    liff.init({ liffId: '1657559918-NqzQz1GA' }, () => {
+    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
       if (liff.isLoggedIn()) {
         runApp();
       } else {
@@ -25,36 +25,38 @@ function App() {
       }
     }, err => console.error(err));
   }
+
   const runApp = () => {
     const idToken = liff.getIDToken();
-    idToken = idToken;
+    setIdToken(idToken);
     liff.getProfile().then(profile => {
       console.log(profile);
-      setDisplayName(profile.displayName)
-      setPictureUrl(profile.pictureUrl)
-      setStatusMessage(profile.statusMessage)
-      setUserId(profile.userId)
+      setDisplayName(profile.displayName);
+      setPictureUrl(profile.pictureUrl);
+      setStatusMessage(profile.statusMessage);
+      setUserId(profile.userId);
     }).catch(err => console.error(err));
   }
 
   useEffect(() => {
-    initLine()
-  }, [])
-
+    initLine();
+  }, []);
 
   return (
     <div className="App">
-      <div style={{ textAlign: 'center' }}>
-        <h1>React with LINE Login</h1>
-        <hr />
-        <img src={pictureUrl} width="300px" height="300px" />
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>id token: </b> {idToken}</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>display name: </b> {displayName}</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>status message: </b> {statusMessage}</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginRight: '20%' }}><b>user id: </b> {userId}</p>
+      <header className="App-header">
+      <div style={{ textAlign: "center" }}>
+        <h1>React with LINE Login test bot1</h1>
+        <hr/>
+        <img src={pictureUrl} width="300px" height="300px"/>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>id token: </b> {idToken}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>status message: </b> {statusMessage}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
 
-        <button onClick={() => logout()} style={{ width: '80%', height: '30px' }}>Logout</button>
+        <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
       </div>
+      </header>
     </div>
   );
 }
