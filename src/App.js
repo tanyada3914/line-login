e3a274f855c9887b652ecf63ebf48ca948b7effe
puriinz <puriinz@puriinzs-MacBook-Pro.local>
2022-10-16 18:30:07 +0700

    test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }
 
   const initLine = () => {
-    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
+    liff.init({ liffId: '1657559918-NqzQz1GA' }, () => {
       if (liff.isLoggedIn()) {
         runApp();
       } else {
